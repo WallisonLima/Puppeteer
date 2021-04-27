47ad50e1d4b93312835eca18aaaa1b02e720a3a1
Wallison Lima <wallisonlimaoliveira@gmail.com>
2021-04-27 17:24:10 -0300

    Tentando implementar Typescript no projeto

--- robot.tsx
+++ robot.tsx
@@ -1,23 +1,24 @@
 const puppeteer = require('puppeteer')
+
 async function ra() {
-	return new Promise(async (resolve, reject)=>{
+	return new Promise(async (resolve, reject) => {
 		const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
 		let urls = [
 			'https://linkedin.com/WallisonLima/',
 			'https://github.com/WallisonLima/Puppeteer'
 		];
 		let i = 0
-		for(let each of urls) {
+		for (let each of urls) {
 			console.log(each)
 			let page = await browser.newPage();
 			await page.setViewport({ width: 1920, height: 1080 });
 			await page.goto(each);
-			await page.screenshot({path: 'screenshot'+i+'.png'});
+			await page.screenshot({ path: 'screenshot' + i + '.png' });
 			i++
 		}
-		browser.close()
-		resolve()
+		resolve(browser.close())
 	})
-	
+
 }
 ra();
+
